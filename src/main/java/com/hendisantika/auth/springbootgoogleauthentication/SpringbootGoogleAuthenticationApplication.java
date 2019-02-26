e53ad69e17f9702f6cec25b583d6275e63f4ffee
hendisantika <hendisantika@yahoo.co.id>
2019-02-27 06:26:54 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/auth/springbootgoogleauthentication/SpringbootGoogleAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.auth.springbootgoogleauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGoogleAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGoogleAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/auth/springbootgoogleauthentication/SpringbootGoogleAuthenticationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.auth.springbootgoogleauthentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootGoogleAuthenticationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
