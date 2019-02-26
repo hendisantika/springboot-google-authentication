63f6cce3eae8e076d0fbda2fd217dbbeede91c0d
hendisantika <hendisantika@yahoo.co.id>
2019-02-27 06:38:41 +0700

    #10 - Add Authentication Status Enum

---
+++ src/main/java/com/hendisantika/auth/springbootgoogleauthentication/domain/AuthenticationStatus.java
@@ -0,0 +1,14 @@
+package com.hendisantika.auth.springbootgoogleauthentication.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-google-authentication
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-02-27
+ * Time: 06:38
+ */
+public enum AuthenticationStatus {
+    AUTHENTICATED, REQUIRE_TOKEN_CHECK, FAILED
+}
