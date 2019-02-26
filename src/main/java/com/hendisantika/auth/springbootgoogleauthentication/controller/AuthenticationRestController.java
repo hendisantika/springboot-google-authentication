7afad15b62d3a0e1d537f846f35a0942e26eefe4
hendisantika <hendisantika@yahoo.co.id>
2019-02-27 06:39:27 +0700

    #11 - Add Authentication Rest Controller

---
+++ src/main/java/com/hendisantika/auth/springbootgoogleauthentication/controller/AuthenticationRestController.java
@@ -0,0 +1,76 @@
+package com.hendisantika.auth.springbootgoogleauthentication.controller;
+
+import com.hendisantika.auth.springbootgoogleauthentication.domain.AuthenticationStatus;
+import com.hendisantika.auth.springbootgoogleauthentication.domain.User;
+import com.hendisantika.auth.springbootgoogleauthentication.service.TotpService;
+import com.hendisantika.auth.springbootgoogleauthentication.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-google-authentication
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-02-27
+ * Time: 06:38
+ */
+@RestController
+@RequestMapping(value = "/authenticate/", method = RequestMethod.POST)
+public class AuthenticationRestController {
+    @Value("${2fa.enabled}")
+    private boolean isTwoFaEnabled;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private TotpService totpService;
+
+    @RequestMapping(value = "{login}/{password}", method = RequestMethod.POST)
+    public AuthenticationStatus authenticate(@PathVariable String login, @PathVariable String password) {
+        Optional<User> user = userService.findUser(login, password);
+
+        if (!user.isPresent()) {
+            return AuthenticationStatus.FAILED;
+        }
+        if (!isTwoFaEnabled) {
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(login, password);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return AuthenticationStatus.AUTHENTICATED;
+        } else {
+            SecurityContextHolder.getContext().setAuthentication(null);
+            return AuthenticationStatus.REQUIRE_TOKEN_CHECK;
+        }
+    }
+
+    @RequestMapping("token/{login}/{password}/{token}")
+    public AuthenticationStatus tokenCheck(@PathVariable String login, @PathVariable String password, @PathVariable String token) {
+        Optional<User> user = userService.findUser(login, password);
+
+        if (!user.isPresent()) {
+            return AuthenticationStatus.FAILED;
+        }
+
+        if (!totpService.verifyCode(token, user.get().getSecret())) {
+            return AuthenticationStatus.FAILED;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user.get().getLogin(), user.get().getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return AuthenticationStatus.AUTHENTICATED;
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public void logout() {
+        SecurityContextHolder.clearContext();
+    }
+}
